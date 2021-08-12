c1da0fa65cd731afa6299e74e4dc140a9d345a87
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-08-12 06:10:19 +0000

    Support specifying ES env
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- scripts/confluence.sh
+++ scripts/confluence.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-./confluence --confluence-url='https://wiki.lfnetworking.org' --confluence-es-url="`cat ./secrets/ES_URL.prod.secret`" --confluence-user="`cat ./secrets/user.secret`" --confluence-token="`cat ./secrets/token.secret`" $*
+# ESENV=prod|test
+if [ -z "${ESENV}" ]
+then
+  ESENV=test
+fi
+./confluence --confluence-url='https://wiki.lfnetworking.org' --confluence-es-url="`cat ./secrets/ES_URL.${ESENV}.secret`" --confluence-user="`cat ./secrets/user.secret`" --confluence-token="`cat ./secrets/token.secret`" $*
