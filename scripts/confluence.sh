2219fbed2628c55b252def0851be16543a3361c7
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-08-12 06:03:40 +0000

    Add SSO params
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- scripts/confluence.sh
+++ scripts/confluence.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./confluence --confluence-url='https://wiki.lfnetworking.org' --confluence-es-url="`cat ./secrets/ES_URL.prod.secret`" $*
+./confluence --confluence-url='https://wiki.lfnetworking.org' --confluence-es-url="`cat ./secrets/ES_URL.prod.secret`" --confluence-user="`cat ./secrets/user.secret`" --confluence-token="`cat ./secrets/token.secret`" $*
