cdac44bd8d0df289bcde52f929f26e45e6705c7c
Łukasz Gryglicki <lgryglicki@cncf.io>
2022-02-24 12:56:10 +0000

    Update
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- scripts/encrypt_example.sh
+++ scripts/encrypt_example.sh
@@ -8,7 +8,8 @@ export ESURL="`cat ./secrets/ES_URL.prod.secret`"
 export STREAM=''
 export CONFLUENCE_NO_INCREMENTAL=1
 # curl -s -XPOST -H 'Content-Type: application/json' "${ESURL}/last-update-cache/_delete_by_query" -d'{"query":{"term":{"key.keyword":"Confluence:https://wiki.lfnetworking.org"}}}' | jq -rS '.' || exit 1
-../insights-datasource-github/encrypt "`cat ./secrets/user.secret`" > ./secrets/user.encrypted.secret || exit 2
-../insights-datasource-github/encrypt "`cat ./secrets/token.secret`" > ./secrets/token.encrypted.secret || exit 3
-./confluence --confluence-url='https://wiki.riscv.org' --confluence-debug=1 --confluence-es-url="${ESURL}" --confluence-user="`cat ./secrets/user.encrypted.secret`" --confluence-token="`cat ./secrets/token.encrypted.secret`" --confluence-stream="${STREAM}" $* 2>&1 | tee run.log
-#./confluence --confluence-url='https://wiki.lfai.foundation' --confluence-debug=1 --confluence-es-url="${ESURL}" --confluence-stream="${STREAM}" $* 2>&1 | tee run2.log
+#../insights-datasource-github/encrypt "`cat ./secrets/user.secret`" > ./secrets/user.encrypted.secret || exit 2
+#../insights-datasource-github/encrypt "`cat ./secrets/token.secret`" > ./secrets/token.encrypted.secret || exit 3
+#./confluence --confluence-url='https://wiki.riscv.org' --confluence-debug=0 --confluence-es-url="${ESURL}" --confluence-user="`cat ./secrets/user.encrypted.secret`" --confluence-token="`cat ./secrets/token.encrypted.secret`" --confluence-stream="${STREAM}" $* 2>&1 | tee run.log
+#./confluence --confluence-url='https://wiki.lfai.foundation' --confluence-debug=0 --confluence-es-url="${ESURL}" --confluence-stream="${STREAM}" $* 2>&1 | tee run2.log
+./confluence --confluence-url='https://wiki.openmainframeproject.org' --confluence-debug=1 --confluence-es-url="${ESURL}" --confluence-stream="${STREAM}" $* 2>&1 | tee run3.log
