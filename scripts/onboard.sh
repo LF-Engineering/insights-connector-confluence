40642861f9e1a175f93a434649d224d52a1d3f76
Łukasz Gryglicki <lgryglicki@cncf.io>
2022-03-01 14:28:04 +0000

    post merge fix
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- scripts/onboard.sh
+++ scripts/onboard.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ -z "$1" ]
+if [ -z "${1}" ]
 then
   echo "$0: you need to specify confluence URL as a 1st argument"
   exit 1
@@ -8,4 +8,4 @@ if [ -z "$TOKEN" ]
 then
   TOKEN="`./scripts/get_token.sh`"
 fi
-curl -s -XPOST -H 'Content-Type: application/json' -H "Authorization: Bearer ${TOKEN}" 'https://api-gw.dev.platform.linuxfoundation.org/insights-service/v2/connectors/confluence' -d"{\"confluence_url\":\"https://wiki.tungsten.io\"}" | jq -rS '.'
+curl -s -XPOST -H 'Content-Type: application/json' -H "Authorization: Bearer ${TOKEN}" 'https://api-gw.dev.platform.linuxfoundation.org/insights-service/v2/connectors/confluence' -d"{\"confluence_url\":\"${1}\"}" | jq -rS '.'
