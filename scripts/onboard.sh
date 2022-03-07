23cc3fa538381415b959676bd94c0d063b6be36c
Łukasz Gryglicki <lgryglicki@cncf.io>
2022-03-07 13:15:01 +0000

    One more script update
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- scripts/onboard.sh
+++ scripts/onboard.sh
@@ -8,4 +8,9 @@ if [ -z "$TOKEN" ]
 then
   TOKEN="`./scripts/get_token.sh`"
 fi
+if [ -z "$TOKEN" ]
+then
+  echo "$0: no TOKEN specified, existing"
+  exit 2
+fi
 curl -s -XPOST -H 'Content-Type: application/json' -H "Authorization: Bearer ${TOKEN}" 'https://api-gw.dev.platform.linuxfoundation.org/insights-service/v2/connectors/confluence' -d"{\"confluence_url\":\"${1}\"}" | jq -rS '.'
