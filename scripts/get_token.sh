862ab1dda974ff7449c647d3c77c4449689f2e20
Łukasz Gryglicki <lgryglicki@cncf.io>
2022-03-07 13:13:44 +0000

    Fix a typo
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- scripts/get_token.sh
+++ scripts/get_token.sh
@@ -3,7 +3,7 @@ cookie="`cat ./secrets/cookie.secret`"
 client_id="`cat ./secrets/client_id.secret`"
 client_secret="`cat ./secrets/client_secret.secret`"
 audience="`cat ./secrets/audience.secret`"
-if ( [ -z "${cookie}" ] || [ -z "${client_id}" ] || [ -z "${client_secret}" ] || [-z "${audience}" ] )
+if ( [ -z "${cookie}" ] || [ -z "${client_id}" ] || [ -z "${client_secret}" ] || [ -z "${audience}" ] )
 then
   echo "$0: you need to have a ./secrets/cookie.secret, ./secrets/client_id.secret, ./secrets/client_secret.secret and ./secrets/audience.secret files"
   exit 1
